922024c1ae0938388ecbd2f98f9cb564a976cd72
AmirAbidjanov <amirabidjonov@gmail.com>
2026-05-29 18:40:03 +0500

    done

---
+++ task11.py
@@ -0,0 +1,14 @@
+class Vehicle:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def move(self):
+        print(f"{self.brand} {self.model} is moving.")
+
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+ 
